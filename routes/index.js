c8f85100d652ae4ba15a4c5cddd83c78b076fcb3
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-02-08 11:14:56 +0100

    Css, html och query

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,26 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Welcome' })
+  res.render('index.njk', { title: 'Game reviews' })
+})
+
+router.get('/search', function (req, res) {
+  console.log(req.query.q)
+  // then use searchQuery in SQL
+  // should filter to check so its only letters and numbers
+  /*
+  SELECT * FROM jens_movie_director
+  JOIN jens_movie ON jens_movie_director.movie_id = jens_movie.id
+  JOIN jens_director ON jens_movie_director.director_id = jens_director.id
+  WHERE jens_movie.title LIKE "%QUERY%";
+*/
+
+  // SELECT * FROM gabriel_game JOIN gabriel_gamereview ON gabriel_gamereview.game_id = gabriel_game.game_id
+
+  let query = req.query.q
+  query = query.replace(/[^a-zA-Z0-9]/g, '')
+
+  res.render('search.njk', { title: 'Search', query: query })
 })
 
 module.exports = router
