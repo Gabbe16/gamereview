e97a1314502e2741db492dad5eb0d36e1c42f79c
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-02-15 11:13:21 +0100

    db, routes changes and new views

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,79 @@ router.get('/', function (req, res) {
 
 const pool = require('../db')
 
-router.get('/dbtest/:id', async (req, res) => {
+router.get('/reviews', async (req, res) => {
   try {
     const id = req.params.id
-    const [reviews] = await pool .promise().query(`SELECT * FROM gabriel_reviews JOIN gabriel_games ON gabriel_reviews.game_id = gabriel_games.id WHERE game_id = ${id}`)
+    const [reviews] = await pool .promise().query(`SELECT * FROM gabriel_reviews JOIN gabriel_games ON gabriel_reviews.game_id = gabriel_games.id`)
     console.log(reviews)
     res.render('reviews.njk', {
-      title: reviews[0].title,
+      title: 'Alla spelrecensioner',
       reviews: reviews,
       author: "Gabriel"
     })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/reviews/new', async function (req, res) {
+  try {
+    const [reviews] = await pool.promise().query('SELECT * FROM gabriel_reviews')
+    return res.render('newreview.njk', {
+      title: 'Ny review',
+    })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/reviews/:id/delete', async function (req, res) {
+  try {
+    const [result] = await pool.promise().query(
+      `DELETE FROM gabriel_reviews WHERE id = ?`,
+      [req.params.id]
+    )
+    res.redirect('/reviews')
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+// De två routes nedanför behöver fortfarande ändras
+
+router.get('/reviews/:id', async function (req, res) {
+  try {
+    const [catsWithBreed] = await pool.promise().query(
+      `SELECT jens_cat.*, jens_cat_breed.name as breed, jens_cat_breed.description
+      FROM jens_cat 
+      JOIN jens_cat_breed
+      ON jens_cat.breed_id = jens_cat_breed.id
+      WHERE jens_cat.id = ?`, [req.params.id]
+    );
+    // jag måste ange att jag vill ha första elementet i arrayen,
+    // annars får jag en array med en katt
+    return res.render('review.njk', {
+      title: 'Katt - ' + catsWithBreed[0].name,
+      cat: catsWithBreed[0]
+    })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+router.post('/reviews', async function (req, res) {
+  // res.json(req.body) för att kolla och kika den data vi får från front-end
+  try {
+    const [result] = await pool.promise().query(
+      `INSERT INTO jens_cat (name, age, breed_id)
+      VALUES (?, ?, ?)`,
+      [req.body.name, req.body.age, req.body.breed]
+    )
+    res.redirect('/reviews')
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
